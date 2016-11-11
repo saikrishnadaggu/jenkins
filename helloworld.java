6d53af01f47059f5c7b5f03896c2ba477f477fb1
saikrishnadaggu <daggu.saikrishna2398@gmail.com>
2016-11-11 12:51:06 -0600

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+/**
+* @author saikrishna
+* @parm args
+*/
+public static void main(string[]args){
+//TODO Auto-generated method stub
+Greeting obj=new greeting();
+system.out.println(obj.great("jenkins"));
+}
+}
